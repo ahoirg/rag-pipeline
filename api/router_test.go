a8c0bc0dc3390f11dfab05e5d413916397a4e3ea
Ali <alihaydarozdag@gmail.com>
2025-11-11 22:26:32 +0100

    refactored code in router_test

--- api/router_test.go
+++ api/router_test.go
@@ -3,13 +3,31 @@ package api
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"rag-pipeline/models"
 	"testing"
+
+	"gopkg.in/yaml.v3"
 )
 
 func TestCreateRAGRouter(t *testing.T) {
+	path := filepath.Clean("../config.yaml") //TODO
+
+	raw_config, err := os.ReadFile(path)
+	if err != nil {
+		t.Errorf("TestCreateRAGRouter FAIL: readfile, detail: %s", err.Error())
+	}
+
+	var config models.Config
+	if err := yaml.Unmarshal([]byte(raw_config), &config); err != nil {
+		t.Errorf("TestCreateRAGRouter FAIL: failed to convert raw_config to yaml, detail: %s", err.Error())
+	}
 
 	// Initialize dependencies before creating router
-	//InitApiDependencies()
+	if err := InitApiDependencies(&config); err != nil {
+		t.Errorf("GET /api/ping: expected 200 OK, got %s", err.Error())
+	}
 
 	// Create the router
 	r := CreateRAGRouter()
